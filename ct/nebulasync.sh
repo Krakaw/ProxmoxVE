4d9e4332df328306e5c1ba6b8e8b75ded3b8fe71
Krakaw <41575888+Krakaw@users.noreply.github.com>
2025-03-24 05:39:29 -0700

    Added generator and nebula sync

---
+++ ct/nebulasync.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Krakaw
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/lovelaze/nebula-sync
+
+# App Default Values
+APP="Nebula Sync"
+var_tags="dns;pihole"
+var_cpu="1"
+var_ram="512"
+var_disk="4"
+var_os="debian"
+var_version="12"
+var_unprivileged="1"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+    header_info
+    check_container_storage
+    check_container_resources
+
+    if [[ ! -f /opt/${APP}_version.txt ]]; then
+        msg_error "No ${APP} Installation Found!"
+        exit
+    fi
+
+    RELEASE=$(curl -s https://api.github.com/repos/lovelaze/nebula-sync/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+    if [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
+        msg_info "Stopping Service"
+        systemctl stop ${APP,,}
+        msg_ok "Stopped Service"
+
+        msg_info "Updating ${APP} to v${RELEASE}"
+        wget -q "https://github.com/lovelaze/nebula-sync/releases/download/${RELEASE}/nebula-sync_0.7.0_linux_amd64.tar.gz/${RELEASE}}"
+        $STD dpkg -i "nebula-sync_0.7.0_linux_amd64.tar.gz/${RELEASE}}"
+        rm -f "nebula-sync_0.7.0_linux_amd64.tar.gz/${RELEASE}}"
+        echo "${RELEASE}" >"/opt/${APP}_version.txt"
+        msg_ok "Updated ${APP} to v${RELEASE}"
+
+        msg_info "Starting Service"
+        systemctl start ${APP,,}
+        msg_ok "Started Service"
+    else
+        msg_ok "No update required. ${APP} is already at v${RELEASE}"
+    fi
+    exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:80${CL}"
